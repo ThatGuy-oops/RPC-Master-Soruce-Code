27af591c74b15a25bcb6a1b6ec6b8682e15d6d0a
ThatGuy-oops <feedpro181@gmail.com>
2022-08-25 17:41:10 -0400

    fixed

---
+++ confg.js
@@ -0,0 +1,4 @@
+module.exports = {
+    BOT_INIVTE: 'https://discord.com/api/oauth2/authorize?client_id=1002330889096794314&permissions=2416371792&scope=applications.commands%20bot',
+    BOT_ID: '1002330889096794314',
+}
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const RPC = require('discord-rpc')
+const { BOT_INVITE, BOT_ID } = require("./confg")
 const rpc = new RPC.Client({
     transport: 'ipc'
 })
 
 rpc.on("ready", () => {
     rpc.setActivity({
-        details: "Spidey has over 60+ commands",
+        details: "Advanced ticket bot!",
         state: "Upvote My Bot",
         largeImageKey: "Large",
-        largeImageText: ";)",
+        largeImageText: "TicketBot",
         startTimestamp: new Date(),
-        buttons : [{label : "Bot Invite Link" , url : "https://discord.com/api/oauth2/authorize?client_id=831272139708563469&permissions=8&scope=bot"}, {label : "Upvote" , url : "https://top.gg/bot/831272139708563469/vote"}]
+        buttons : [{label : "Bot Invite Link" , url : `${BOT_INVITE}`}, {label : "Upvote" , url : `https://top.gg/bot/${BOT_ID}/vote`}]
     })
 
     console.log("successfully activated the RPC")
 })
 
 rpc.login({
-    clientId: '831272139708563469'
+    clientId: '1002330889096794314'
 })
